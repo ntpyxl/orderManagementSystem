c28b75cec7da31389fbefbd8ec02f9939d077a22
ntpyxl <abayafrancis.e@gmail.com>
2025-10-26 18:33:28 +0800

    Styled scrollbars

--- src/admin/index.php
+++ src/admin/index.php
@@ -14,6 +14,7 @@ if(!isset($_SESSION['cashier_id'])) {
     <title>Pixelshop - Admin Dashboard</title>
 
     <link href="../styles.css" rel="stylesheet">
+    <link href="./scrollbarStyles.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
